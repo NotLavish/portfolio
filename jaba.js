c3cfb1ccc6526083f521ec9fe95baba2151c1a93
Lavish <notlavish@gmail.com>
2021-12-27 20:47:24 +0530

    fade through tabs

--- jaba.js
+++ jaba.js
@@ -1,9 +1,10 @@
 function display(classnamehaha){
     var activern = document.getElementsByClassName("active")[0];
-    activern.classList.remove("active");
     activern.classList.add("inactive");
+    activern.classList.remove("active");
     classnamehaha = classnamehaha.split("#")[1]
     var clicked = document.getElementById(classnamehaha);
-    clicked.classList.add("active");
     clicked.classList.remove("inactive");
+    clicked.classList.add("active");
+    
 }
