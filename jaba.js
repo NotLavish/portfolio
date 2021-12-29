ccf0c754ca470d61fa3668d306cb3853b6a3a07a
Lavish <notlavish@gmail.com>
2021-12-29 23:08:42 +0530

    added more pictures

--- jaba.js
+++ jaba.js
@@ -1,3 +1,7 @@
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+  }
+
 function display(classnamehaha){
     var activern = document.getElementsByClassName("active")[0];
     activern.classList.add("inactive");
@@ -8,3 +12,12 @@ function display(classnamehaha){
     clicked.classList.add("active");
     
 }
+
+
+function poggies_selector() {
+    var poggies = ["poggies/1.png", "poggies/2.png", "poggies/3.png"];
+    var poggies = pickRandom(poggies);
+    document.getElementById("poggies").setAttribute("src", poggies);
+}
+
+poggies_selector()
